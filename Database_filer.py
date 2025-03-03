488aa2125a7646d2c23b414c8b74310cd524392e
JOSPHATT <kimwelemutunga@gmail.com>
2025-03-03 21:05:46 +0300

    Update Database_filer.py

--- Database_filer.py
+++ Database_filer.py
@@ -5,18 +5,18 @@ f=finished_games()
 # Import MySQL Connector/Python 
 import mysql.connector as connector
 
-db_pass='Araf'
+#db_pass='Araf'
 
 # Connect to the database
 try:
     print("Establishing a new connection between MySQL and Python.")
-    connection=connector.connect(user="root",password=db_pass)
+    connection=connector.connect(user="root",password='Araf')
     print("A connection between MySQL and Python is successfully established")
 
 except connector.Error as er:
     print("Error code:", er.errno)
     print("Error message:", er.msg)
-connection=connector.connect(user="root",password=db_pass)
+connection=connector.connect(user="root",password='Araf')
 
 
 # Create a cursor object to communicate with entire MySQL database
