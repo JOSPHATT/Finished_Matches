0d613f5218f53ffd315e3ba9299a61405bb522e1
JOSPHATT <kimwelemutunga@gmail.com>
2025-03-26 14:53:49 +0300

    Update Database_Main.py

--- Database_Main.py
+++ Database_Main.py
@@ -1,9 +1,9 @@
-import Main_Engine_2 as M_E_2
+from Main_Engine_2 import Finished_Games
 import pandas as pd
 import sqlite3
 
 # define connection and cursor
-M_E_2
+Finished_Games=Finished_Games
 connection = sqlite3.connect('finished_matches.db')
 
 cursor = connection.cursor()
@@ -11,8 +11,8 @@ cursor = connection.cursor()
 # create activities table
 
 command1 = """CREATE TABLE IF NOT EXISTS
-activities(id INTEGER PRIMARY KEY, timestamp INTEGER, 
-datetime TEXT, duration REAL, distance REAL)"""
+finished_games(id INTEGER, timestamp TEXT, 
+HOME TEXT, AWAY TEXT, HOME_G INTEGER, AWAY_G INTEGER)"""
 
 cursor.execute(command1)
 
